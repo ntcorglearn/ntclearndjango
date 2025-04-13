5e919d848b868f13473746523544d78593bcbb86
TEJ <tejdhamala25@gmail.com>
2025-04-13 12:38:26 +0545

    Added Tej Dhamala app and message

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include,path
+from django.http import HttpResponse
+
 
 urlpatterns = [
+    path('tejapp/', include("tejapp.urls")),
     path('admin/', admin.site.urls),
 ]
