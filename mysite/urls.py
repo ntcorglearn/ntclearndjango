e756c227b8fdafeac76216425f4a9341d21fba6a
sanjeevdroid <thesanjeeva@gmail.com>
2025-04-13 13:07:56 +0545

    Update urls.py//

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("bishal/", include("bishal.urls")),
     path('admin/', admin.site.urls),
+    path('sghimire/",include("sghimire.urls")),
 ]
